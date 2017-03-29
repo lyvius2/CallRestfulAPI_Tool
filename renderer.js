4c5818302d104d6e5b4256f21af25c7c724e295a
walter.hwang <lyvius2@naver.com>
2017-03-29 21:09:12 +0900

    기본 렌더러 프로세스 및 화면 구현

---
+++ renderer.js
@@ -0,0 +1,19 @@
+const shell = require('electron').shell
+const links = document.querySelectorAll('a[href]')
+
+Array.prototype.forEach.call(links, function (link) {
+	const url = link.getAttribute('href')
+	if (url.indexOf('http') === 0) {
+		link.addEventListener('click', function (e) {
+			e.preventDefault()
+			shell.openExternal(url)
+		})
+	}
+})
+
+function init () {
+	document.querySelector('.js-nav').classList.add('is-shown')
+	document.querySelector('.js-content').classList.add('is-shown')
+}
+
+init()
