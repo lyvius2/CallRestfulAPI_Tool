67ecb6cada6d634444a93af8b7df4502b72effb5
walter.hwang <lyvius2@naver.com>
2017-04-04 19:29:56 +0900

    get method 호출 시 params를 queryString으로 보내도록 변경

--- py/requester.py
+++ py/requester.py
@@ -12,14 +12,14 @@ class ApiError(Exception):
 def request_api(method, url, params):
     try:
         if method == 'get':
-            res = requests.get(url, params=params)
+            res = requests.get(url + '?' + params)
         else:
-            res = requests.post(url, data=params)
-        if res.status_code == 200:
+            res = requests.post(url, data=eval(params))
+        if res.status_code <= 400:
             return res.text
         else:
             raise ApiError('Response Status Code : %s' % res.status_code)
     except Exception as e:
         return e
 
-print(ascii(request_api(sys.argv[1], sys.argv[2], eval(sys.argv[3]))))
+print(ascii(request_api(sys.argv[1], sys.argv[2], sys.argv[3])))
